bfe3a6b82721cf60569741f027661a9d46d1205a
Oleksii30 <polyanovskyyol@gmail.com>
2020-05-09 23:42:21 +0300

    Updates

---
+++ precache-manifest.e6ab9d1f77bcc91c3794bb5cec4c8e1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf55d0c802dbf2eb629a1553877e9fca",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fd83f91378b2b38c2a2f",
+    "url": "/monsters/static/css/main.dc34ec8f.chunk.css"
+  },
+  {
+    "revision": "cb80a15f71da0f888782",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.668f8c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd83f91378b2b38c2a2f",
+    "url": "/monsters/static/js/main.d2adb567.chunk.js"
+  },
+  {
+    "revision": "3ad366ebbc5ea22c619f",
+    "url": "/monsters/static/js/runtime-main.735d0d8b.js"
+  }
+]);
