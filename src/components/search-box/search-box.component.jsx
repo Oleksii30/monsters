c98acd44949e1aff8d0c16901a83f09d67db6f11
Oleksii30 <polyanovskyyol@gmail.com>
2020-05-09 23:45:47 +0300

    adding files for githubpages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './search-box.styles.css'
+
+const SearchBox = props =>{
+    return(
+        <input
+        className = 'search' 
+        type = 'search' 
+        placeholder='search monsters' 
+        onChange = {props.change}/>
+    )
+}
+
+export default SearchBox
